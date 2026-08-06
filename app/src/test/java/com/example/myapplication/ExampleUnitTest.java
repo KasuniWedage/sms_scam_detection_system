3f9962345125f7ada37633541cecfc85fb47cbab
senuri58821116 <asp2022111@as.rjt.ac.lk>
2026-08-07 01:19:07 +0530

    Add import statement for JUnit
    Added additional import for JUnit testing.

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import org.junit.Test;
 
+
 import static org.junit.Assert.*;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
